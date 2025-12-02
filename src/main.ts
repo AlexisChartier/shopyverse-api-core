cfb200fd500fbc73c506001dc7a65533482f7bff
Lucas Vignau <lucasvignau@MacBook-Air-de-Lucas.local>
2025-12-02 19:06:44 +0100

    add create dto

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3001); // ⚠️ change le port du back
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
